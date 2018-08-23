b23d8df6a4dae063a893e204a3bb5aef5e01a905
Ricardo Alex da Costa soares <ricardoalexsoares@hotmail.com>
2018-08-23 17:04:26 -0300

    -criando as pastas de backup -Renomeando a atual para o nome com data e hora -Testado incluindo alterações nas pastas e rodando o programa -Essa versão considera a existência da pasta sistema dentro da pasta backup para iniciar o processo.

--- BackupDeploy/Program.cs
+++ BackupDeploy/Program.cs
@@ -3,13 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace BackupDeploy
 {
     class Program
     {
-        static void Main(string[] args)
+        public static  string RetornaNomePasta()
         {
+            string sdata;
+            DateTime data;
+            data = System.DateTime.Now;
+            sdata = Convert.ToString(data);
+            sdata = sdata.Replace("/", "");
+            sdata = sdata.Replace(":", "");
+            sdata = sdata.Replace(" ", "_");
+            sdata = "Sistema_" + sdata;
+            return sdata;
+        }
+
+        public static void CopiarArquivos(string caminhoorigem, string caminhodestino)
+        {
+                       
+            string[] diretorios = System.IO.Directory.GetDirectories(caminhoorigem, "*", SearchOption.AllDirectories);
+            
+
+            foreach (string d in diretorios)
+            {                
+                //Console.WriteLine(d);                
+                Directory.CreateDirectory(d.Replace(caminhoorigem, caminhodestino));
+                //Console.WriteLine(d.Replace(caminhoorigem, caminhodestino) + " criado");                
+            }
+
+            foreach (string f in Directory.GetFiles(caminhoorigem, "*.*", SearchOption.AllDirectories))
+            {
+                File.Copy(f, f.Replace(caminhoorigem, caminhodestino), true);
+                //Console.WriteLine(f.Replace(caminhoorigem,caminhodestino));                    
+
+            }
+
+        }
+
+        static void Main(string[] args)
+        {            
+            string caminhodapastaorigem = @"D:\controle\Tiplan\sistema\";
+            string caminhodapastadestino = @"D:\controle\Tiplan\backup\sistema\";
+            string caminhodapastadestinorenomeado;
+            string nomedapastacomdata;
+
+            if (!Directory.Exists(caminhodapastadestino))
+            {
+                Directory.CreateDirectory(caminhodapastadestino);
+                CopiarArquivos(caminhodapastaorigem, caminhodapastadestino);
+            }
+            else
+            {
+                nomedapastacomdata = RetornaNomePasta();
+                caminhodapastadestinorenomeado = caminhodapastadestino.Replace("sistema", nomedapastacomdata);
+                Directory.Move(caminhodapastadestino, caminhodapastadestinorenomeado);
+                //Console.WriteLine(caminhodapastadestinorenomeado);
+                CopiarArquivos(caminhodapastaorigem, caminhodapastadestino);
+            }            
+
+            //Console.WriteLine(sdata);
+            Console.ReadLine();
+
         }
     }
 }
